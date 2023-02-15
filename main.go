2cabeb37f9667a997a7fe9f82755599418f59684
Ali Azam <94777254+agzg@users.noreply.github.com>
2023-02-15 21:13:44 +0500

    write main.go

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Point struct {
+	X, Y int
+}
+
+func (p Point) String() string {
+	return fmt.Sprintf("(%d, %d)", p.X, p.Y)
+}
+
+func (p Point) Distance(q Point) float64 {
+	x := math.Pow(float64(p.X-q.X), 2)
+	y := math.Pow(float64(p.Y-q.Y), 2)
+
+	return math.Sqrt(x + y)
+}
+
+func main() {
+	p := Point{0, 3}
+	q := Point{4, 0}
+
+	fmt.Println(p, q)
+	fmt.Println(p.Distance(q))
+}
